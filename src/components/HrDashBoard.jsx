1162a184880919b448b98c4ef61faa0486e0c0e5
Nallapatiakhila <akhilanallapati3@gmail.com>
2026-05-30 18:38:52 +0530

    Update HrDashBoard.jsx

--- src/components/HrDashBoard.jsx
+++ src/components/HrDashBoard.jsx
@@ -16,7 +16,7 @@ const HrDashboard = () => {
 
   useEffect(() => {
     const email = localStorage.getItem("email") || "";
-    axios.get(`http://localhost:8080/api/hr/all-bookings?email=${email}`)
+    axios.get(`https://driverio-backend-1.onrender.com/api/hr/all-bookings?email=${email}`)
       .then(res => {
         const bookings = res.data;
         const total = bookings.length;
